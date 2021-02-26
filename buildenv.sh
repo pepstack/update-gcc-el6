ad157c9e23fd0691526bb3a3d0e62b0a596c3a20
pepstack <350137278@qq.com>
2021-02-26 16:26:49 +0800

    update

--- buildenv.sh
+++ buildenv.sh
@@ -6,4 +6,3 @@ export CC=/usr/local/gcc/bin/gcc
 export CXX=/usr/local/gcc/bin/g++
 export PATH=/usr/local/gcc/bin:/usr/local/cmake/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/gcc/lib64:/usr/local/gmp/lib:/usr/local/mpfr/lib:/usr/local/mpc/lib:$LD_LIBRARY_PATH
-
